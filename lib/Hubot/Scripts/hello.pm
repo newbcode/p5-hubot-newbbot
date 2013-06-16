3f5969e5fcfd7b5adab39b3938b5544bc95f412c
newbcode <newbcode@newb-dtop>
2013-06-16 16:21:47 +0900

    Modify fonts path and POD

--- lib/Hubot/Scripts/hello.pm
+++ lib/Hubot/Scripts/hello.pm
@@ -20,14 +20,13 @@ sub hello {
     my $sender = $msg->message->user->{name};
 
     my @fonts = qw/banner block big bubble digital ivrit lean mini mnemonic
-        script shadow slant small smscript smshadow smslant standard term
-        letter mini mnemonic smascii12 smascii9/;
+        script shadow slant small smscript smshadow smslant standard term/;
 
     my $user_input = $msg->match->[0];
 
     my $num = int( rand(21) );
 
-    my $font = Text::FIGlet->new(-d=>"/usr/share/figlet", -f=>"$fonts[$num]");
+    my $font = Text::FIGlet->new(-d=>"./figlet", -f=>"$fonts[$num]");
     my $text = $font->figify(-A=>"$user_input");
     $msg->send("Font is $fonts[$num]!!!!!!!!!!!!");
     $msg->send( split (/\n/, $text) );
@@ -39,17 +38,21 @@ sub hello {
 
 =head1 Name 
 
-    Hubot::Scripts::weather
+    Hubot::Scripts::hello
  
 =head1 SYNOPSIS
- 
-    weather <city name>  - View current local area weather information. 
-    weather weekly <city name> - View weekly local area weather information.
-    weather weekly <city name1> <city name2>... - View weekly local areas weather information.
-    weather forecast <local name> - View local weather forecast information. (ex: KangWon-Do, Gyeonggi-Do ..)
 
+    hello <text> - Random show text in ascii art
+ 
 =head1 AUTHOR
 
     YunChang Kang <codenewb@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2013 by Yunchang Kang.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself
  
 =cut
